dd6990c63924bd33a192cb2807e6fd61bdb44f98
Vuong_Huu_Thien <vuonghuuthientk@gmail.com>
2021-02-05 01:12:36 +0700

    Owl Carousel O

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,67 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'owl-carousel';
+  images_1: any;
 
-  mySlideImages = ['../assets/images/image1.jpg','../assets/images/image2.jpeg','../assets/images/image3.jpg'];
-  myCarouselImages =['../assets/images/image1.jpg','../assets/images/image2.jpeg','../assets/images/image3.jpg'];
-  
-  mySlideOptions={items: 1, dots: true, nav: true};
-  myCarouselOptions={items: 3, dots: true, nav: true};
+  mySlideImages = [
+    '../assets/images/demo_0.jpg',
+    '../assets/images/demo_1.jpg',
+    '../assets/images/demo_2.jpg',
+    '../assets/images/demo_3.jpg',
+    '../assets/images/demo_4.jpg',
+    '../assets/images/demo_5.jpg',
+    '../assets/images/demo_6.jpg',
+    '../assets/images/demo_7.jpg'
+  ];
+  myCarouselImages = [
+    '../assets/images/demo_0.jpg',
+    '../assets/images/demo_1.jpg',
+    '../assets/images/demo_2.jpg',
+    '../assets/images/demo_3.jpg',
+    '../assets/images/demo_4.jpg',
+    '../assets/images/demo_5.jpg',
+    '../assets/images/demo_6.jpg',
+    '../assets/images/demo_7.jpg'
+  ];
+
+  mySlideOptions = { items: 4, dots: true, nav: true };
+  myCarouselOptions = { items: 3, dots: true, nav: true };
+
+  //
+  images = [
+    { image: '../assets/images/demo_0.jpg' },
+    { image: '../assets/images/demo_1.jpg' },
+    { image: '../assets/images/demo_2.jpg' },
+    { image: '../assets/images/demo_3.jpg' },
+    { image: '../assets/images/demo_4.jpg' },
+    { image: '../assets/images/demo_5.jpg' },
+    { image: '../assets/images/demo_6.jpg' },
+    { image: '../assets/images/demo_7.jpg' },
+  ];
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { OwlModule } from 'ngx-owl-carousel';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    OwlModule
+    OwlModule,
+    CarouselModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
